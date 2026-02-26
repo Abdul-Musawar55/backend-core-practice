e6d7c1c569c1cb1c1e7e35e773d0ff4f40adc0f3
Your Name <you@example.com>
2026-02-26 12:19:09 +0500

    Updated new changes

--- routes/user.route.js
+++ routes/user.route.js
@@ -13,5 +13,3 @@ router.get("/single/:id", getUserById);
 router.delete("/:id", deleteUser);
 
 module.exports = router;
-
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5OWZlYTcyNzk3ZGRkOTA2YjMxNWZjYiIsImlhdCI6MTc3MjA4ODA2MCwiZXhwIjoxNzcyNjkyODYwfQ.7Gi3DNLcm5pTLe53stsCx5Zk-1TcsmsAesszZDReGR8
